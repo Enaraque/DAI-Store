59cf089f04d092382e7ffa48655ba82ee4205406
Enrique Araque <minixute@gmail.com>
2023-10-22 12:49:47 +0200

    Modify urls and views files

--- e-commerce/etienda/urls.py
+++ e-commerce/etienda/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("categorias/<str:categoria>", views.get_categoria, name="categorias"),
+    path("filtrar", views.filtrar_busqueda, name="filtro"),
     ]
--- e-commerce/etienda/views.py
+++ e-commerce/etienda/views.py
@@ -5,22 +5,31 @@ from .models import Database_connection, Producto
 
 
 def index(request):
-    template = loader.get_template("consultas.html")
     tienda_db, client = Database_connection().get_connection()
     productos_collection = Database_connection().get_collection("productos")
-    consulta1 = Producto.get_producto(productos_collection,
+
+    consultas = get_consultas(productos_collection)
+    categoriasEncabezado = get_categorias_encabezado(productos_collection)
+    context = {**consultas, **categoriasEncabezado}
+
+    return render(request, "consultas.html", context)
+
+
+def get_consultas(collection):
+
+    consulta1 = Producto.get_producto(collection,
                                       "electronics",
                                       100,
                                       200
                                       )
-    consulta2 = Producto.get_product_type(productos_collection, "pocket")
-    consulta3 = Producto.get_product_by_rate(productos_collection, 4)
-    consulta4 = Producto.get_producto(productos_collection,
+    consulta2 = Producto.get_product_type(collection, "pocket")
+    consulta3 = Producto.get_product_by_rate(collection, 4)
+    consulta4 = Producto.get_producto(collection,
                                       "men's clothing",
                                       orden="rating.rate"
                                       )
-    consulta5 = Producto.get_facturacion(productos_collection)
-    consulta6 = Producto.get_facturacion(productos_collection, "category")
+    consulta5 = Producto.get_facturacion(collection)
+    consulta6 = Producto.get_facturacion(collection, "category")
     context = {
         "consulta1": consulta1,
         "consulta2": consulta2,
@@ -29,5 +38,34 @@ def index(request):
         "consulta5": consulta5,
         "consulta6": consulta6
         }
+    return context
 
-    return HttpResponse(template.render(context, request))
+
+def get_categorias_encabezado(coleccion):
+    categorias = Producto.get_all_categories(coleccion)
+    context = {"categorias": categorias}
+
+    return context
+
+
+def get_categoria(request, categoria):
+    tienda_db, client = Database_connection().get_connection()
+    productos_collection = Database_connection().get_collection("productos")
+
+    productos = Producto.get_producto(productos_collection, categoria)
+    encabezado = get_categorias_encabezado(productos_collection)
+    context = {"productos": productos, **encabezado}
+
+    return render(request, "categorias.html", context)
+
+
+def filtrar_busqueda(request):
+    tienda_db, client = Database_connection().get_connection()
+    productos_collection = Database_connection().get_collection("productos")
+    filtrado = request.GET.get('search')
+
+    productos = Producto.get_product_type(productos_collection, filtrado)
+    encabezado = get_categorias_encabezado(productos_collection)
+    context = {"productos": productos, **encabezado}
+
+    return render(request, "categorias.html", context)
